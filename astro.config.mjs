65057e41eec844062a4b0e9781c48e9801886d7e
javier <elteliblejvi@gmail.com>
2024-01-09 17:00:24 +0100

    finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), react(), mdx()]
+});
